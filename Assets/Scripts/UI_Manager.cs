888edd7c505163ac76863cead86bfa322df1a3f5
Joannehuang11 <chwong0314@me.com>
2022-04-10 18:51:44 -0400

    feat(*): Add ambient sound for two weather

--- Assets/Scripts/UI_Manager.cs
+++ Assets/Scripts/UI_Manager.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI_Manager : MonoBehaviour
 {
     public GameObject RainFall;
     public Light DirectLight;
+    public GameObject Audio;
+    public AudioClip Sun;
+    public AudioClip Rain;
 
 
     // Start is called before the first frame update
@@ -22,15 +26,16 @@ public class UI_Manager : MonoBehaviour
 
     public void changeToRain()
     {
-        RainFall.SetActive(true); 
+        RainFall.SetActive(true);
         DirectLight.intensity = 0.5f;
-
+        Audio.GetComponent<AudioSource>().clip = Rain;
+        Audio.GetComponent<AudioSource>().Play();
     }
-
     public void changeToSun()
     {
         RainFall.SetActive(false);
         DirectLight.intensity = 1.0f;
-
+        Audio.GetComponent<AudioSource>().clip = Sun;
+        Audio.GetComponent<AudioSource>().Play();
     }
 }
